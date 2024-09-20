b3c9ce773b255513d26b43853fc3a48aed389408
Danila Platonov <danilaplatonov209@gmail.com>
2024-09-20 10:03:06 +0300

    text change after changing theme has been added

--- src/components/pages/Admin.tsx
+++ src/components/pages/Admin.tsx
@@ -18,7 +18,9 @@ export default function AdminPage (props: AdminPageProps) {
     return (
         <div className='adminPage__main'>
             <div className="admin__menu"> 
-                <div className="item">Пользователи 1234</div>
+                <div className="item">Пунк меню 1</div>
+                <div className="item">Пунк меню 2</div>
+                <div className="item">Пунк меню 3</div>
             </div>
             <div className="content">Контент: {children}</div>
         </div>
