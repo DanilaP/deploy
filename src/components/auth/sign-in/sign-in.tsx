aaedb2084497f948bf2fd8e8ac7248b5b0750f0f
Danila Platonov <danilaplatonov209@gmail.com>
2024-11-01 10:27:47 +0300

    styles has been fixed

--- src/components/auth/sign-in/sign-in.tsx
+++ src/components/auth/sign-in/sign-in.tsx
@@ -1,4 +1,3 @@
-import * as React from 'react';
 import { useEffect, useState } from 'react';
 import { Button, FormControl, FormLabel, Link, TextField } from '@mui/material';
 import { validator } from '../../../helpers/auth-helpers';
--- src/components/auth/sign-up/sign-up.tsx
+++ src/components/auth/sign-up/sign-up.tsx
@@ -1,4 +1,3 @@
-import * as React from 'react';
 import { useEffect, useState } from 'react';
 import { Button, FormControl, FormLabel, Link, TextField } from '@mui/material';
 import { IUser } from '../../../interfaces/interfaces';
