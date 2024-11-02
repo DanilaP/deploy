4911d2b745637944a8e43a8c2246eccd5dade276
Danila Platonov <danilaplatonov209@gmail.com>
2024-11-02 11:27:41 +0300

    breadcrumbs has been added

--- src/components/breadcrumbs/bread-crumbs.tsx
+++ src/components/breadcrumbs/bread-crumbs.tsx
@@ -3,15 +3,21 @@ import Breadcrumbs from '@mui/material/Breadcrumbs';
 import useBreadcrumbs from 'use-react-router-breadcrumbs';
 import { adminRoutes, routes } from '../../routes';
 import { Link } from 'react-router-dom';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 export default function BreadCrumbs () {
 
     const allRoutes = [...routes, ...adminRoutes];
     const breadcrumbs = useBreadcrumbs<any>(allRoutes);
+    const [fixedBreadCrumbs, setFixedBreadCrumbs] = useState(breadcrumbs);
 
     useEffect(() => {
-        console.log(breadcrumbs);
+        const fixedArrayOfBreadCrumbs = breadcrumbs.filter(({ breadcrumb, match } )=> {
+            if (!match.params.hasOwnProperty("*")) {
+                return { breadcrumb, match };
+            }
+        });
+        //setFixedBreadCrumbs(fixedArrayOfBreadCrumbs);
     }, [breadcrumbs]);
     
     return (
