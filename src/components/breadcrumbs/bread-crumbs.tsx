75e3d26c976bb8c7b78e4a9a074fa032558333b8
Danila Platonov <danilaplatonov209@gmail.com>
2024-11-02 14:36:29 +0300

    breadcrumbs has been added

--- src/components/breadcrumbs/bread-crumbs.tsx
+++ src/components/breadcrumbs/bread-crumbs.tsx
@@ -2,36 +2,50 @@ import './bread-crumbs.scss';
 import Breadcrumbs from '@mui/material/Breadcrumbs';
 import useBreadcrumbs from 'use-react-router-breadcrumbs';
 import { adminRoutes, routes } from '../../routes';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import { useEffect, useState } from 'react';
 
 export default function BreadCrumbs () {
 
-    const allRoutes = [...routes, ...adminRoutes];
-    const breadcrumbs = useBreadcrumbs<any>(allRoutes);
-    const [fixedBreadCrumbs, setFixedBreadCrumbs] = useState(breadcrumbs);
+    const location = useLocation();
+    const breadcrumbs = useBreadcrumbs<any>([...routes, ...adminRoutes]);
+    const [fixedBreadCrumbs, setFixedBreadCrumbs] = useState<any>([]);
 
     useEffect(() => {
-        const fixedArrayOfBreadCrumbs = breadcrumbs.filter(({ breadcrumb, match } )=> {
-            if (!match.params.hasOwnProperty("*")) {
-                return { breadcrumb, match };
+        const fixedArrayOfBreadCrumbs = breadcrumbs.reduce((prev: any[], el: any) => {
+            if (el.match.params.hasOwnProperty("*")) {
+                return prev;
+            } else {
+                if (Object.keys(el.match.params).length !== 0) {
+                    return [
+                        ...prev,
+                        { ...el, breadcrumb: {
+                            ...el.breadcrumb,
+                            props: {
+                                ...el.breadcrumb.props,
+                                children: el.breadcrumb.props.children + " " + Object.values(el.match.params)[0]
+                            }
+                        } }
+                    ];
+                }
+                return [...prev, el];
             }
-        });
-        //setFixedBreadCrumbs(fixedArrayOfBreadCrumbs);
-    }, [breadcrumbs]);
+        }, []);
+        setFixedBreadCrumbs(fixedArrayOfBreadCrumbs);
+    }, [location.pathname]);
     
     return (
         <div className='bread-crumbs'>
             <Breadcrumbs>
-            {
-                breadcrumbs.map(({ breadcrumb, match }) => (
-                    <div key={ match.pathname }>
-                        <Link to={ match.pathname }>
-                            { breadcrumb }
-                        </Link>
-                    </div>
-                ))
-            }
+                {
+                    fixedBreadCrumbs.map(({ breadcrumb, match }) => (
+                        <div key={ match.pathname }>
+                            <Link className='bread-crumbs-link' to={ match.pathname }>
+                                { breadcrumb }
+                            </Link>
+                        </div>
+                    ))
+                }
             </Breadcrumbs>
         </div>
     );
