73a8672cb257cb9ac15018f26f77e6b9e2564000
EgorPlat <egorplatonov2002@gmail.com>
2024-11-27 08:06:04 +0300

    create-velidators-for-phone-and-email

--- src/helpers/validators-helper.ts
+++ src/helpers/validators-helper.ts
@@ -3,5 +3,20 @@ const validateRequiredField = (value: any) => {
     if (value.length === 0) return false;
     return true;
 };
+const validateEmail = (email: string) => {
+    return email.match(
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    );
+};
+const validateWebsiteRef = (email: string) => {
+    return email.match(
+        /^(https?:\/\/)?([\w-]{1,32}\.[\w-]{1,32})[^\s@]*/
+    );
+};
+const validatePhone = (phone: string) => {
+    const cleanedTel = phone.replace(/\D/g, '');
+    const russianTelLength = 11;
+    return cleanedTel.length === russianTelLength;
+};
 
-export { validateRequiredField };
+export { validateRequiredField, validateEmail, validatePhone, validateWebsiteRef };
