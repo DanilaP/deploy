63a890e879c3197ee365cead7c551805f452ab23
EgorPlat <egorplatonov2002@gmail.com>
2024-12-11 13:03:45 +0300

    add-basic-layout-for-main-pages

---
+++ src/components/layouts/main-layout/main.tsx
@@ -0,0 +1,37 @@
+import { Button, TextField } from "@mui/material";
+import "./main.scss";
+import { useNavigate } from "react-router";
+
+interface IMainLayoutProps {
+    children: React.ReactElement | null
+}
+
+export default function MainLayout({ children }: IMainLayoutProps) {
+
+    const navigate = useNavigate();
+
+    const handleNavigateToCatalog = () => {
+        navigate("/shop");
+    };
+
+    return (
+        <div className="main-layout">
+            <div className="main-menu">
+                <Button
+                    variant="outlined"
+                    className="catalog-button"
+                    onClick={ handleNavigateToCatalog }
+                >
+                    Каталог
+                </Button>
+                <TextField
+                    className="search-input"
+                    placeholder="Поиск по сайту"
+                />
+            </div>
+            <div className="main-children">
+                { children }
+            </div>
+        </div>
+    );
+}
